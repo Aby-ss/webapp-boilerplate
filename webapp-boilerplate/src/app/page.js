583c3b931711e56cba0ea5a7f4d6484226f11235
Rao <103417697+Aby-ss@users.noreply.github.com>
2024-07-17 16:19:21 +0000

    [feat] Added basic landing page elements ✅

---
+++ webapp-boilerplate/src/app/page.js
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="font-extrabold text-3xl lg:text-5xl tracking-tight md:-mb-4 text-center pt-20">
+        Homemade Web Application,
+        <br/>
+        for fast deployment
+      </h1>
+
+      <p className="font-medium text-2xl text-center w-[650px] pb-40 text-[20px] leading-[24px]">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+      </p>
+
+      <div className="pb-50">
+      <ul className="list-none">
+        <li className="flex items-center"><span className="mr-2">✅</span> Lorem Ipsum</li>
+        <li className="flex items-center"><span className="mr-2">✅</span> Lorem Ipsum</li>
+        <li className="flex items-center"><span className="mr-2">✅</span> Lorem Ipsum</li>
+      </ul>
+    </div>
+    </main>
+  );
+}
